7abacc2ccf08240ee0d7037ea32c7e0bb79623fe
黄俊 <sky.huangjun@1data.info>
2024-02-02 13:45:22 +0800

    添加openai chat的请求私有化部署的模型

--- examples/openai_chatcompletion_client.py
+++ examples/openai_chatcompletion_client.py
@@ -1,8 +1,9 @@
+# _*_ coding: utf-8 _*_
 from openai import OpenAI
 
 # Modify OpenAI's API key and API base to use vLLM's API server.
 openai_api_key = "EMPTY"
-openai_api_base = "http://localhost:8000/v1"
+openai_api_base = "http://10.10.106.240:8000/v1"
 
 client = OpenAI(
     # defaults to os.environ.get("OPENAI_API_KEY")
@@ -12,26 +13,32 @@ client = OpenAI(
 
 models = client.models.list()
 model = models.data[0].id
-
+stream = True
 chat_completion = client.chat.completions.create(
     messages=[{
         "role": "system",
         "content": "You are a helpful assistant."
     }, {
         "role": "user",
-        "content": "Who won the world series in 2020?"
-    }, {
-        "role":
-        "assistant",
-        "content":
-        "The Los Angeles Dodgers won the World Series in 2020."
-    }, {
-        "role": "user",
-        "content": "Where was it played?"
+        "content": "使用python,写一个贪吃蛇的程序"
     }],
+    stop=["<|endoftext|>"],
     model=model,
+    stream=stream
 )
 
 
-print("Chat completion results:")
-print(chat_completion)
+if not stream:
+    print("Chat completion results:")
+    print(chat_completion)
+    print(chat_completion.choices[0].message.content)
+else:
+    result_text = ''
+    for info in chat_completion:
+        content = info.choices[0].delta.content
+        if content is None:
+            continue
+
+        result_text = result_text + content
+
+    print(result_text)
